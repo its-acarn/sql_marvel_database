01f36b3ba5baf73365121744fbfe5f8709d78dd5
svvishjs <andreww92@live.com>
2020-09-28 16:25:55 +0100

    MVP complete

--- marvel.sql
+++ marvel.sql
@@ -249,8 +249,32 @@ INSERT INTO movies (title, year, show_time) VALUES ('Black Panther', 2018, '21:0
 
 -- 9.  The cinema would like to make the Iron Man movies a triple billing. Find out the show time of "Iron Man 2" and set the show time of "Iron Man 3" to start two hours later.
 
-SELECT title from movies WHERE title = 'Iron Man 2';
+-- SELECT title from movies WHERE title = 'Iron Man 2';
 
-UPDATE movies set show_time = '20:45' WHERE title = 'Iron Man 3';
+-- UPDATE movies set show_time = '20:45' WHERE title = 'Iron Man 3';
 
-SELECT * from movies;
+-- SELECT * from movies;
+
+
+--  id |                title                | year | show_time 
+-- ----+-------------------------------------+------+-----------
+--   1 | Iron Man                            | 2008 | 17:00
+--   2 | The Incredible Hulk                 | 2008 | 23:55
+--   3 | Iron Man 2                          | 2010 | 18:45
+--   4 | Thor                                | 2011 | 15:45
+--   5 | Captain America: The First Avenger  | 2011 | 14:15
+--   6 | Avengers Assemble                   | 2012 | 14:45
+--   8 | Thor: The Dark World                | 2013 | 22:55
+--   9 | Batman Begins                       | 2005 | 13:40
+--  10 | Captain America: The Winter Soldier | 2014 | 18:25
+--  11 | Guardians of the Galaxy             | 2014 | 13:10
+--  12 | Avengers: Age of Ultron             | 2015 | 20:20
+--  13 | Ant-Man                             | 2015 | 13:00
+--  14 | Captain America: Civil War          | 2016 | 12:35
+--  15 | Doctor Strange                      | 2016 | 22:00
+--  16 | Guardians of the Galaxy 2           | 2017 | 14:05
+--  17 | Spider-Man: Homecoming              | 2017 | 23:00
+--  18 | Thor: Ragnarok                      | 2017 | 22:10
+--  19 | Black Panther                       | 2018 | 21:00
+--   7 | Iron Man 3                          | 2013 | 20:45
+-- (19 rows)
